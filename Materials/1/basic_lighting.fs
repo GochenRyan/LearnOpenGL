b672398ad4bd3f869f6cc269a7d81e04045418c3
GochenRyan <rangaocheng@outlook.com>
2019-08-01 13:57:57 +0800

    在片段着色器中创建了一个材质结构体的uniform，修改一下光照的计算来顺应新的材质属性

---
+++ Materials/1/basic_lighting.fs
@@ -0,0 +1,37 @@
+#version 330 core
+in vec3 Normal;
+in vec3 FragPos;
+
+out vec4 FragColor;
+
+struct Material{
+	vec3 ambient;
+	vec3 diffuse;
+	vec3 specular;
+	float shininess;
+};
+uniform Material material;
+uniform vec3 lightColor;
+uniform vec3 lightPos;
+uniform vec3 viewPos;
+
+void main()
+{
+	// 环境光
+    vec3 ambient = lightColor * material.ambient;
+
+    // 漫反射 
+    vec3 norm = normalize(Normal);
+    vec3 lightDir = normalize(lightPos - FragPos);
+    float diff = max(dot(norm, lightDir), 0.0);
+    vec3 diffuse = lightColor * (diff * material.diffuse);
+
+    // 镜面光
+    vec3 viewDir = normalize(viewPos - FragPos);
+    vec3 reflectDir = reflect(-lightDir, norm);  
+    float spec = pow(max(dot(viewDir, reflectDir), 0.0), material.shininess);
+    vec3 specular = lightColor * (spec * material.specular);  
+
+    vec3 result = ambient + diffuse + specular;
+    FragColor = vec4(result, 1.0);
+}
